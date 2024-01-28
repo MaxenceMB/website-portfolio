c051d53af695e2971c41cbc172893808e97c80bd
Maxence <maury.maxence13@gmail.com>
2024-01-29 00:23:49 +0100

    hamburger. ham-ham-ham, bur-bur-bur, ger-ger-ger.

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -7,6 +7,26 @@ Ce fichier JS est commun pour toutes les pages du site web
 
 ***********************************************************/
 
+const hamburger = document.querySelector("#hamburger");
+const nav       = document.querySelector("nav");
+const navMenu   = document.querySelector("#menu_links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav_link").forEach(l => l.addEventListener("click", () => { 
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    nav.classList.remove("active");
+}));
+
+function test() {
+    alert("Test fonctionnel.");
+}
+
 var tabLinks = document.getElementsByClassName("about-tab-title");
 var tabContents = document.getElementsByClassName("about-tab-content");
 
@@ -21,50 +41,3 @@ function openAboutTab(tabName) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
 }
-
-function test() {
-    alert("Test fonctionnel.");
-}
-
-function translation() {
-    let btn = document.getElementById("language-button");
-
-    if (btn.innerText == "English") {
-        btn.innerText = "Français";
-
-        document.querySelectorAll('.fr').forEach(function(el) {
-            el.style.display = 'none';
-        });
-        document.querySelectorAll('.en').forEach(function(el) {
-            el.style.display = 'inline-block';
-        });
-
-        document.getElementsByClassName("about-tab-title")[3].classList.add("active-link");
-        document.getElementById("about-skills-en").classList.add("active-tab");
-    } else {
-        btn.innerText = "English";
-
-        document.querySelectorAll('.fr').forEach(function(el) {
-            el.style.display = 'inline-block';
-        });
-        document.querySelectorAll('.en').forEach(function(el) {
-            el.style.display = 'none';
-        });
-
-        document.getElementsByClassName("about-tab-title")[0].classList.add("active-link");
-        document.getElementById("about-skills-fr").classList.add("active-tab");
-    }
-}
-
-// window.onscroll = function() {myFunction()};
-
-// var header = document.getElementById("myHeader");
-// var sticky = header.offsetTop;
-
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// } 
