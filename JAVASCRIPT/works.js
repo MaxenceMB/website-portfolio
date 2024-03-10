1c57b82133758aeee8d9577e3c913c86b6aba6f3
Maxence <maury.maxence13@gmail.com>
2024-03-10 13:16:46 +0100

    Finished 'works' section prototype

--- JAVASCRIPT/works.js
+++ JAVASCRIPT/works.js
@@ -1,94 +1,135 @@
-const mainDiv   = document.querySelector("#works");
-const descDiv   = document.querySelector("#works-desc");
-const galeryDiv = document.querySelector("#works-galery");
-const mediasDiv = document.querySelector("#works-galery-media");
-const sliderDiv = document.querySelector("#works-slider");
+/***********************************************************
 
+@Author: Maxence MAURY-BALIT
+Fichier JavaScript pour mon site PortFolio max-mb.com
+
+Ce fichier JavaScript est fait pour gérer la partie "works"
+du site.
+
+***********************************************************/
+
+// Toutes les divs utilisées après
+const mainDiv       = document.querySelector("#works");
+const descDiv       = document.querySelector("#works-desc");
+const carousselDiv  = document.querySelector("#works-caroussel");
+const mediasDiv     = document.querySelector("#works-media");
+const sliderDiv     = document.querySelector("#works-slider");
+const dotsDiv       = document.querySelector("#works-dots");
+
+// Mes travaux
 var works = [
-    {"id"    : 0, 
-     "title" : "titre test 1",
-     "desc"  : "Test description. Lorem ipsum dolor sit amet.",
-     "img"   :  {
-                    0: "3.mp4",
-                    1: "2.png",
-                    2: "1.png",
-                    3: "3.mp4"
-                }
+    {   "id"        : 0, 
+        "title"     : "titre test 1",
+        "desc"      : "Test description. Lorem ipsum dolor sit amet.",
+        "thumbnail" : "0.png",
+        "medias"    : {
+                        0: "0.png",
+                        1: "2.png",
+                        2: "1.png",
+                        3: "3.mp4"
+                    }
     },
 
-    {"id"    : 1, 
-     "title" : "titre test 2",
-     "desc"  : "Test description. Lorem ipsum dolor sit amet.",
-     "img"   :  {
-                    0: "1.png",
-                    1: "2.png",
-                    2: "0.png"
-                }
+    {   "id"        : 1, 
+        "title"     : "OAAAAAAAAA",
+        "desc"      : "Test description. Lorem ipsum dolor sit amet.",
+        "thumbnail" : "1.png",
+        "medias"    : {
+                        0: "1.png",
+                        1: "2.png",
+                        2: "0.png"
+                    }
     },
 
-    {"id"    : 2, 
-     "title" : "titre test 3",
-     "desc"  : "Test description. Lorem ipsum dolor sit amet.",
-     "img"   :  {
-                    0: "2.png",
-                    1: "0.png",
-                    2: "1.png"
-                }
-    },
+    {   "id"        : 2, 
+        "title"     : "titre test 3",
+        "desc"      : "Test description. Lorem ipsum dolor sit amet.",
+        "thumbnail" : "2.png",
+        "medias"    : {
+                        0: "2.png",
+                        1: "0.png",
+                        2: "1.png"
+                    }
+    }
 ];
 
-function workOption(work) {
-    return "<div class = 'works-slider-option' onclick = showWork("+ work["id"] +")><img class = 'works-slider-option-img' src = 'MEDIAS/WORKS/" + work["img"][0] + "'></div>";
+function firstLoad() {
+    showWork(0);
+    for(i = 0; i < works.length; i++) {
+        $(sliderDiv).append(workOption(works[i]));
+    }
 }
 
 function showWork(id) {
     if($(mainDiv).attr("data-work") != id) {
-        $(descDiv).find("h3").fadeOut(   100, function() { $(descDiv).find("h3").text(works[id]["title"]); $(descDiv).find("h3").fadeIn(100);});
-        $(descDiv).find("p").fadeOut(    100, function() { $(descDiv).find("p").text(works[id]["desc"]); $(descDiv).find("p").fadeIn(100);});
-
-        showMedia(works[id]["img"][0].slice(-3), id, 0);
+        updateWorkText(id);
+        updateDots(id);
+        showMedia(id, 0);
 
         $(mainDiv).attr("data-work", id);
-        $(galeryDiv).attr("data-img", 0);
+        $(carousselDiv).attr("data-img", 0);
+    }
+}
+
+function updateWorkText(id) {
+    $(descDiv).find("h3").fadeOut(100, function() { $(descDiv).find("h3").text(works[id]["title"]); $(descDiv).find("h3").fadeIn(100);});
+    $(descDiv).find("p").fadeOut(100, function() { $(descDiv).find("p").text(works[id]["desc"]); $(descDiv).find("p").fadeIn(100);});
+}
+
+function updateDots(id) {
+    $(dotsDiv).empty();
+
+    mediasNumber = Object.keys(works[id]["medias"]).length;
+    for(i = 0; i < mediasNumber; i++) {
+        $(dotsDiv).append(imageOption(works[id], i));
     }
 }
 
 function changeImage(step) {
     let nextStep    = 0;
-    let actualStep  = $(galeryDiv).attr("data-img");
+    let actualStep  = $(carousselDiv).attr("data-img");
     let actualWork  = $(mainDiv).attr("data-work");
-    let numberSteps = Object.keys(works[actualWork]["img"]).length;
+    let numberSteps = Object.keys(works[actualWork]["medias"]).length;
 
     nextStep = mod((+actualStep + +step), +numberSteps);
-
-    let nextFormat = works[actualWork]["img"][nextStep].slice(-3);
-    showMedia(nextFormat, actualWork, nextStep);
-
-    $(galeryDiv).attr("data-img", nextStep);
+    showMedia(actualWork, nextStep);
 }
 
-function showMedia(format, work, number) {
-    $(mediasDiv).fadeOut(100, function() { 
-        if(format == "png") {
-            $("#works-video").hide();
-            $("#works-img").show();
-            $("#works-img").attr("src", "MEDIAS/WORKS/" + works[work]["img"][number]);
-        } else {
-            $("#works-img").hide();
-            $("#works-video").show();
-            $("#works-video").find("source").attr("src", "MEDIAS/WORKS/" + works[work]["img"][number]);
-        }         
-        $(mediasDiv).fadeIn(100);
-    });
+function showMedia(work, number) {
+    if($(carousselDiv).attr("data-img") != number || $(mainDiv).attr("data-work") != work) {
+        $(mediasDiv).fadeOut(100, function() { 
+            $(".selected:first").removeClass("selected");
+            let format = works[work]["medias"][number].slice(-3);
+
+            if(format == "png") {
+                $("#works-video").hide();
+                $("#works-img").attr("src", "MEDIAS/WORKS/" + works[work]["medias"][number]);
+                $("#works-img").show();
+            } else {
+                $("#works-img").hide();
+                $("#works-video > source").attr("src", "MEDIAS/WORKS/" + works[work]["medias"][number]);
+                $("#works-video")[0].load();
+                $("#works-video").show();
+            }         
+
+            $(carousselDiv).attr("data-img", number);
+            $(".works-dot:nth-child(" + (+number+1) + ")").addClass("selected");
+            $(mediasDiv).fadeIn(100);
+        });
+    }
+}
+
+function imageOption(work, number) {
+    return "<div class = 'works-dot' onclick = showMedia("+ work["id"] + '\,'  + number +")>&#x2022;</div>";
+}
+
+function workOption(work) {
+    return "<div class = 'works-slider-option' onclick = showWork("+ work["id"] +")><img class = 'works-slider-option-img' src = 'MEDIAS/WORKS/" + work["thumbnail"] + "'></div>";
 }
 
 function mod(n, m) {
     return ((n % m) + m) % m;
 }
 
-showWork(0);
-changeImage(0);
-for(i = 0; i < works.length; i++) {
-    $(sliderDiv).append(workOption(works[i]));
-}
+firstLoad()
 
